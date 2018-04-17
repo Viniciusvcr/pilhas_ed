dfc768a34cf53119ee89049c1fbf8b1578a79c69
ViniciusRegatieri <viniciusvcr@hotmail.com>
2018-04-17 18:29:23 -0300

    Exercícios 1 e 4b e adição da main

--- pilha_dinamica_sentinela.cpp
+++ pilha_dinamica_sentinela.cpp
@@ -77,14 +77,108 @@ int exp_regular(char* expressao, int tam){ //exercício 1
 	return vazia(&A);
 }
 
-void esvazia_pilha(pilha* p){ //exercício 4b
+int esvazia_pilha(pilha* p){ //exercício 4b
 	celula *aux = p->topo->prox;
 	celula *del = aux;
 
-	while(aux != NULL){
-		aux = aux->prox;
-		free(del);
-		del = aux;
+	if(!vazia(p)){
+		while(aux != NULL){
+			aux = aux->prox;
+			free(del);
+			del = aux;
+		}
+		p->topo->prox = NULL;
+		return 1;
 	}
-	p->topo->prox = NULL;
+	return 0;
+}
+
+void clear_screen(){
+	system("cls");
+}
+
+void pause_screen(){
+	system("pause");
+	cout << endl;
+}
+
+int main(){
+	pilha A;
+	int opt;
+	celula insere;
+	item retornos;
+	char expressao[100];
+
+	do{
+		fflush(stdin);
+		clear_screen();
+		cout << "[1] Inicia pilha" << endl;
+		cout << "[2] Pilha vazia?" << endl;
+		cout << "[3] Mostrar pilha" << endl;
+		cout << "[4] Inserir elemento" << endl;
+		cout << "[5] Tirar primeiro" << endl;
+		cout << "[6] Mostrar item do topo" << endl;
+		cout << "[7] Expressao regular" << endl;
+		cout << "[8] Esvaziar pilha" << endl;
+		cin >> opt;
+		switch(opt){
+			case 1;
+				inicializa(&A);
+				cout << "PILHA INICIALIZADA" << endl;
+				pause_screen();
+			break;
+
+			case 2:
+				if(vazia(&A)) cout << "PILHA VAZIA" << endl;
+				else cout << "PILHA NAO VAZIA" << endl;
+				pause_screen();
+			break;
+
+			case 3:
+				clear_screen();
+				cout << "PILHA COMPLETA: ";
+				mostra_pilha(&A);
+				pause_screen();
+			break;
+
+			case 4:
+				clear_screen();
+				cout << "Insira o elemento que deseja inserir: ";
+				cin >> insere.chave;
+				empilha(&A, insere);
+				cout << "ELEMENTO INSERIDO" << endl;
+				pause_screen();
+			break;
+
+			case 5:
+				if(desempilha(&A, &retornos))
+					cout << retornos.chave << " RETIRADO COM SUCESSO" << endl;
+				else cout << "ERRO NA REMOCAO" << endl;
+				pause_screen();
+			break;
+
+			case 6:
+				if(topo(&A, &retornos))
+					cout << "ITEM DO TOPO: " << retornos.chave << endl;
+				else cout << "LISTA VAZIA!" << endl;
+				pause_screen();
+			break;
+
+			case 7:
+				clear_screen();
+				cout << "Escreva a expressao: ";
+				cin >> expressao;
+				if(exp_regular(expressao, strlen(expressao)))
+					cout << "EXPRESSAO CORRETA!" << endl;
+				else cout << "EXPRESSAO INCORRETA" << endl;
+			break;
+
+			case 8:
+				if(esvazia_pilha(&A))
+					cout << "PILHA ESVAZIADA COM SUCESSO!" << endl;
+				else cout << "ERRO NA OPERACAO" << endl;
+				pause_screen();
+			break;
+		}
+	}while(opt != 0);
 }
