14bd90bf24404cb5c6adc50a7a211c332121f143
Vinicius Regatieri <viniciusvcr@hotmail.com>
2018-04-17 20:22:27 -0300

    Main e Novas Funções

--- pilha_estatica.cpp
+++ pilha_estatica.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <stdio.h>
+#include <stdlib.h>
 #define TAM 100
+
+using namespace std;
 
 typedef struct tipo_item{
 	int chave;
@@ -24,7 +27,7 @@ int vazia(pilha* p){
 }
 
 void mostra_pilha(pilha* p){
-	for(int i=0; i<p->topo; i++){
+	for(int i=p->topo-1; i>=0; i--){
 		cout << p->dados[i].chave << " ";
 	}
 	cout << endl;
@@ -48,9 +51,34 @@ int desempilha(pilha* p, item* retorno){
 	return 0;
 }
 
+int topo(pilha* p, item* top){
+	if(!vazia(p)){
+		*top = p->dados[(p->topo)-1];
+		return 1;
+	}
+	return 0;
+}
+
+int esvazia(pilha* p){ //exercício 4a
+	if(!vazia(p)){
+		p->topo = 0;
+		return 1;
+	}
+	return 0;
+}
+
+void clear_screen(){
+	system("cls");
+}
+
+void pause_screen(){
+	system("pause");
+	cout << endl;
+}
+
 int main(){
 	pilha A;
-	item insere;
+	item insere, retorno;
 	int opt;
 
 	do{
@@ -62,9 +90,14 @@ int main(){
 		cout << "[4] Mostra pilha" << endl;
 		cout << "[5] Insere elemento" << endl;
 		cout << "[6] Tira elemento" << endl;
+		cout << "[7] Mostrar item do topo" << endl;
+		cout << "[8] Esvaziar pilha" << endl; 
+		cin >> opt;
 		switch(opt){
 			case 1:
 				inicializa(&A);
+				cout << "PILHA INICIALIZADA" << endl;
+				pause_screen();
 			break;
 
 			case 2:
@@ -81,12 +114,41 @@ int main(){
 
 			case 4:
 				clear_screen();
+				cout << "PILHA COMPLETA: ";
 				mostra_pilha(&A);
 				pause_screen();
 			break;
 
 			case 5:
+				clear_screen();
+				cout << "Digite o elemento que deseja inserir: ";
+				cin >> insere.chave;
+				if(empilha(&A, insere))
+					cout << "ELEMENTO INSERIDO COM SUCESSO" << endl;
+				else cout << "ERRO NA OPERACAO" << endl;
+				pause_screen();
+			break;
+
+			case 6:
+				if(desempilha(&A, &retorno))
+					cout << retorno.chave << " RETIRADO COM SUCESSO" << endl;
+				else cout << "ERRO NA OPERACAO" << endl;
+				pause_screen();
+			break;
 				
+			case 7:
+				if(topo(&A, &retorno))
+					cout << "ITEM DO TOPO: " << retorno.chave << endl;
+				else cout << "ERRO NA OPERACAO" << endl;
+				pause_screen();
+			break;
+
+			case 8:
+				if(esvazia(&A))
+					cout << "PILHA ESVAZIADA" << endl;
+				else cout << "PILHA JA VAZIA" << endl;
+				pause_screen();
+			break;
 		}
 	}while(opt!=0);
 }
